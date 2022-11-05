ed7867624836b6fb6dc8e517bef2cc41437bf6d9
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-11-05 13:07:17 +0000

    experimented with axios inase it was easier than fetch

--- sedna/src/components/pages/IndexPage.js
+++ sedna/src/components/pages/IndexPage.js
@@ -1,19 +1,34 @@
 import React from "react";
 import "./IndexPage.css";
 import { useEffect, useState } from "react";
+import Axios from 'axios'
+import { json } from "react-router-dom";
 
 export const IndexPage = () => {
   const [films, setFilms] = useState([]);
+  const [tags, setTags ] = useState([]);
 
   useEffect(() => {
     fetch("https://6357f067c27556d289325a88.mockapi.io/api/v1/films")
       .then((response) => response.json())
       .then((json) => setFilms(json))
-      
+      // .then((json) => handleResponse(json))
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
+
+  // useEffect(() => {
+  //   fetchFilms();
+  // }, [films]);
+
+  // const fetchFilms = async () => {
+  //   const response = await Axios(
+  //     "https://6357f067c27556d289325a88.mockapi.io/api/v1/films"
+  //   );
+  //   setFilms(response.data);
+    
+  // };
 
   const addTag = (event) => {
     event.preventDefault()
@@ -41,11 +56,12 @@ export const IndexPage = () => {
             <p>{film.description}</p>
             <p>{film.released}</p>
             <p>{film.tags}</p>
-            <form className="addTag" onSubmit = {addTag}>
+            <form className="addTag" onSubmit={addTag}>
               <input
                 className="inputField"
                 name="tag"
                 placeholder="type your tag"
+                onChange={addTag}
               />
               <input className="button" type="Submit" value="Add Tag" />
             </form>
